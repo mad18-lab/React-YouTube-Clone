ceee62a1f7a9a6e198ea0c69ce35cee68e7144ff
Madhav <guptamadhav69@gmail.com>
2024-06-26 16:27:36 +0530

    commit changes

--- src/components/VidRecs.jsx
+++ src/components/VidRecs.jsx
@@ -6,8 +6,8 @@ const VidRecs = ({ sidebar }) => {
     <div className="w-[340px]">
         <div className="flex flex-col gap-2">
             {displaySideCards.map((sideCard) => (
-                <div className="flex gap-1">
-                    <img src={sideCard.url} className="w-24 h-20 rounded-md" alt="/" />
+                <div className="flex gap-2 items-center">
+                    <img src={sideCard.url} className="h-24 rounded-lg object-cover" alt="/" />
                     <div className="flex flex-col gap-1">
                         <h1 className="font-semibold text-sm">{sideCard.title}</h1>
                         <p className="text-sm text-[#313131]">{sideCard.channel}</p>
